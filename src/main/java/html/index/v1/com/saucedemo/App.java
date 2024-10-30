406dbf903b0478ca465c3ec648198c256b20eeea
abdieid <jibriljama1211@gmail.com>
2024-10-30 10:57:24 +0000

    framework Setup

---
+++ src/main/java/html/index/v1/com/saucedemo/App.java
@@ -0,0 +1,10 @@
+package html.index.v1.com.saucedemo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
